d88b8965c642f862f79db7a3ad0e41240c9f4ba0
Peter Nguyen <ptn045@ucsd.edu>
2023-02-25 13:58:26 -0800

    add get and post routes, file-writing functions

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 const express = require("express");
+const bodyParser = require("body-parser");
+const fs = require("fs");
 const PORT = process.env.PORT || 3001;
-const app = express();
 const cors=require("cors");
 const corsOptions ={
    origin: '*', 
@@ -8,10 +9,38 @@ const corsOptions ={
    optionSuccessStatus: 200,
 };
 
-app.use(cors(corsOptions));
+const dataPath = './data/data.json';
+const app = express();
 
+const getData = () => {
+	const jsonData = fs.readFileSync(dataPath);
+	return JSON.parse(jsonData);
+}
+
+const saveData = (data) => {
+	const stringifyData = JSON.stringify(data);
+	fs.writeFileSync(dataPath, stringifyData);
+}
+
+// middleware
+app.use(cors(corsOptions));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true}));
+
+// routes
 app.get("/api", (req, res) => {
-	res.json({ message: "Hello from server!" });
+	const data = getData();
+	res.send(data);
+	// res.json({ message: "Hello from server!" });
+});
+
+app.post("/post/:id", (req, res) => {
+	const accountId = req.params['id'];
+	let existingData = getData();
+	// let newData = [...req.body, ...existingData];
+	existingData[accountId] = req.body;
+	saveData(existingData);
+	res.send(existingData);
 });
 
 app.listen(PORT, () => {
